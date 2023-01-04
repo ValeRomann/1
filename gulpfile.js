8c56d6cb150dc43cfbe18b1576a8360a3d12a60f
valeroman <valeroman@valeroman.com>
2023-01-04 12:38:33 +0300

    This time it's working

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
+
+global.app = {
+  path: path,
+  gulp: gulp
+};
+
+import { copy } from './gulp/tasks/copy.js';
+
+gulp.task('defailt', copy);
