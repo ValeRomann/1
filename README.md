913228f0b9d94302fca5e57ca1ee981b66de127d
valeroman <valeroman@valeroman.com>
2023-01-09 15:45:02 +0300

    Add plugins for dev/build modes. Fix webcss plugin

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 "del": "^6.1.1" downgraded version. for avoiding error.
 
+
 npm install del@6.1.1 --save-dev
+
+
+Error: Cannot find module 'webp-converter/cwebp'
+
+solution (might come in handy):
+copy files from: node_modules> webp-converter> src
+in: node_modules> webp-converter
--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -3,11 +3,13 @@ import plumber from 'gulp-plumber'; //working with errors
 import notify from 'gulp-notify'; //Notifications
 import browsersync from 'browser-sync'; //local server
 import newer from 'gulp-newer'; //check updates
+import ifPlugins from 'gulp-if';
 
 export const plugins = {
   replace: replace,
   plumber: plumber,
   notify: notify,
   browsersync: browsersync,
-  newer: newer
+  newer: newer,
+  if: ifPlugins
 }
